2cbace0fb2f991730452e5273e4076246a93b1cc
Ariana Cai <xc984@nyu.edu>
2019-12-02 23:33:25 +0800

    CUSTOMER

--- main_app/customer.py
+++ main_app/customer.py
@@ -11,6 +11,10 @@ customer_bp = Blueprint("customer", __name__, url_prefix="/cust")
 @customer_bp.route('/customer_page', methods=('POST', 'GET'))
 @login_required
 def search():
+    return render_template('/auth/search_without_login.html', flights=None)
+
+
+def show_result():
     if request.method == "POST":
         time = request.args["time"]
         start_time = time + " " + "00:00:00"
@@ -24,24 +28,16 @@ def search():
                                     "JOIN (SELECT airport_name, city as departure_city FROM Airport) A on Flight.depart_airport = A.airport_name "
                                     "JOIN (SELECT airport_name, city as arrive_city FROM Airport) A2 ON Flight.arrive_airport=A2.airport_name "
                                     "WHERE (depart_date_time BETWEEN ? AND ?) AND depart_airport=? AND arrive_airport=? AND departure_city=? AND arrive_city=?",
-                                    (start_time, end_time, departure_airport, destination_airport, departure_city,
-                                     destination_city)).fetchall()
+                                    (start_time, end_time, departure_airport, destination_airport, departure_city, destination_city)).fetchall()
         for f in target_flights:
             print("here")
             print(f["flight_number"])
-        return render_template('/customer/customer_page.html', flights=target_flights)
+        return render_template('/auth/search_result.html', flights=target_flights)
 
 
 @customer_bp.route('/customer_page', methods=('POST', 'GET'))
 @login_required
 def purchase():
     if request.method == 'POST':
-        db = get_db()
-        target_flights = db.execute("SELECT * FROM Flight "
-                                    "JOIN (SELECT airport_name, city as departure_city FROM Airport) A on Flight.depart_airport = A.airport_name "
-                                    "JOIN (SELECT airport_name, city as arrive_city FROM Airport) A2 ON Flight.arrive_airport=A2.airport_name "
-                                    "WHERE depart_date_time=? AND depart_airport=? AND arrive_airport=? AND departure_city=? AND arrive_city=?",
-                                    (time, departure_airport, destination_airport, departure_city, destination_city))
-
-    return render_template('/cust/customer_page', flights=None)
+        pass
 
